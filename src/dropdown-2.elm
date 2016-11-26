c4f3cbc7cbc52e43ec05a664dac870c04a9f5539
wintvelt <wintvelt@me.com>
2016-11-26 15:00:31 +0100

    add focusedId in model

---
+++ src/dropdown-2.elm
@@ -0,0 +1,93 @@
+{- Part 2, a simple picklist with focus
+-}
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+
+main =
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+-- MODEL
+
+-- our main model, which will change as we use the app
+type alias Model =
+    { pickedFruit : Maybe Fruit
+    , focusedId : Maybe DomID
+    }
+
+-- simple types so we can read the code better
+type alias Fruit = String
+type alias DomID = String
+
+-- global constants/ config
+assortment : List String
+assortment = 
+    [ "Apple"
+    , "Banana"
+    , "Cherry"
+    , "Durian"
+    , "Elderberry"
+    ]
+
+
+
+init : ( Model, Cmd Msg )
+init =
+    { pickedFruit = Nothing
+    } ! []
+
+
+
+-- UPDATE
+
+
+type Msg
+    = FruitPicked Fruit
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    FruitPicked fruit ->
+      { model | pickedFruit = Just fruit } ! []
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- VIEW
+
+
+
+view : Model -> Html Msg
+view model =
+    let
+        itemText =
+            model.pickedFruit
+            |> Maybe.withDefault "-- pick a fruit --" 
+            |> flip String.append " ▾"
+
+    in
+        div []
+            [ p [] [ text <| itemText ] 
+            , ul []
+                (List.map viewFruit assortment)
+            ]
+
+viewFruit : Fruit -> Html Msg
+viewFruit fruit =
+    li [ onClick <| FruitPicked fruit ]
+        [ text fruit ]
