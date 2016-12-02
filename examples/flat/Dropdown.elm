0226baf266b5287c1876612d587944d1aa734fc3
wintvelt <wintvelt@me.com>
2016-12-02 22:50:25 +0100

    make flat example

---
+++ examples/flat/Dropdown.elm
@@ -0,0 +1,96 @@
+module Flat.Dropdown exposing 
+    (Context, Config, view)
+{- a Dropdown component that manages its own state
+-}
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onWithOptions)
+import Json.Decode as Json
+
+import Styles.Styles as Styles
+
+
+
+-- MODEL
+
+{- Context type alias
+this is dynamic stuff - may change in each update cylce
+this is not managed by the dropdown, but passed in from parent
+kind of like props (including callbacks) in react
+-} 
+type alias Context =
+    { selectedItem : Maybe String
+    , isOpen : Bool
+    }
+
+{- Config is the static stuff, that won't change during life cylce
+Like functions and message constructors
+Also transparent, because this is also owned by parent
+-}
+type alias Config msg =
+    { defaultText : String
+    , clickedMsg : msg 
+    , itemPickedMsg : (String -> msg)
+    }
+
+
+-- VIEW
+
+
+
+view : Config msg -> Context -> Model -> List String -> Html msg
+view config context data =
+    let
+        mainText =
+            context.selectedItem
+            |> Maybe.withDefault context.defaultText
+
+        displayStyle =
+            if context.isOpen then
+                ("display", "block")
+            else
+                ("display", "none")
+
+        mainAttr =
+            case data of
+                [] -> 
+                    [ style <| Styles.dropdownDisabled ++ Styles.dropdownInput
+                    ] 
+
+                _ ->
+                    [ style Styles.dropdownInput
+                    , onClick config.clickedMsg 
+                    ] 
+
+    in
+        div 
+            [ style Styles.dropdownContainer ]
+            [ p 
+                mainAttr
+                [ span [ style Styles.dropdownText ] [ text mainText ] 
+                , span [] [ text "▾" ]
+                ]
+            , ul 
+                [ style <| displayStyle :: Styles.dropdownList ]
+                (List.map (viewItem config) data)
+            ]
+            
+    
+viewItem : Context msg -> String -> Html msg
+viewItem context item =
+    li 
+        [ style Styles.dropdownListItem
+        , onClick <| config.itemPickedMsg item
+        ]
+        [ text item ]
+
+
+-- helper to cancel click anywhere
+onClick : msg -> Attribute msg
+onClick message =
+    onWithOptions 
+        "click" 
+        { stopPropagation = True
+        , preventDefault = False
+        }
+        (Json.succeed message)
---
+++ examples/flat/Main.elm
@@ -0,0 +1,174 @@
+{- two dropdowns
+with open/ close state
+and blur when clicked outside
+using a flat approach
+importing a module that exposes config, context and view
+-}
+import Html exposing (..)
+import Html.Attributes exposing (style)
+import Dict exposing (Dict)
+import Json.Decode as Json
+import Mouse exposing (Position)
+
+import Styles.Styles as Styles
+import Flat.Dropdown as Dropdown
+
+main =
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+-- MODEL
+
+-- our main model, which will change as we use the app
+type alias Model =
+    { country : Maybe Country
+    , city : Maybe City
+    , openDropDown : OpenDropDown
+    }
+
+type OpenDropDown =
+    AllClosed
+    | CountryDropDown
+    | CityDropDown
+
+
+init : ( Model, Cmd Msg )
+init =
+    { country = Dropdown.init
+    , city = Dropdown.init
+    , openDropDown = AllClosed
+    } ! []
+
+
+
+-- simple types so we can read the code better
+type alias Country = String
+type alias City = String
+
+-- global constants/ config
+allCities : Dict Country (List City)
+allCities =
+    Dict.fromList
+        [ ("Spain",["Barcelona","Madrid","Alicante","Valencia"])
+        , ("Germany",["Berlin","München","Bonn","Leipzig"])
+        , ("France",["Paris","Lyon","Marseille","Dijon"])
+        , ("Italy",["Florence","Rome","Milan"])
+        ]
+
+countries : List Country
+countries = 
+    Dict.keys allCities
+
+
+countryConfig : Dropdown.Config Msg
+countryConfig =
+    { defaultText = "-- pick a country --"
+    , clickedMsg : Toggle CountryDropDown 
+    , itemPickedMsg : CountryPicked
+    }
+
+cityConfig : Dropdown.Config Msg
+cityConfig =
+    { defaultText = "-- pick a city --"
+    , clickedMsg : Toggle CityDropDown 
+    , itemPickedMsg : CityPicked
+    }
+
+
+
+-- UPDATE
+
+
+type Msg =
+    Toggle OpenDropDown
+    | CountryPicked Country
+    | CityPicked City
+    | Blur Position
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    Toggle dropdown ->
+        if model.openDropDown == dropdown then
+            AllClosed
+        else
+            dropdown
+
+    CountryPicked pickedCountry ->
+        let
+            newCity =
+                if model.country /= Just pickedCountry then
+                    Nothing
+                else
+                    model.city
+
+        in
+            { model 
+            | country = Just pickedCountry
+            , city = newCity
+            , openDropDown = AllClosed
+            } ! []
+
+    CityPicked pickedCity ->
+        { model
+        , city = Just pickedCity
+        , openDropDown = AllClosed
+        } ! []
+
+    Blur _ ->
+        { model
+        | openDropDown = AllClosed
+        } ! []
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    case model.openDropDown of
+        AllClosed ->
+            Sub.none
+
+        _ ->
+            Mouse.clicks Blur
+
+
+
+
+-- VIEW
+
+
+
+view : Model -> Html Msg
+view model =
+    let
+        countryContext =
+            { selectedItem = 
+                model.country |> Maybe.withDefault countryConfig.defaultText
+            , isOpen = model.openDropDown == CountryDropDown
+            }
+
+        cityContext =
+            { selectedItem = 
+                model.city |> Maybe.withDefault cityConfig.defaultText
+            , isOpen = model.openDropDown == CityDropDown
+            }
+
+        cities =
+            model.country
+            |> Maybe.andThen (\c -> Dict.get c allCities)
+            |> Maybe.withDefault []
+
+    in
+        div [ style Styles.mainContainer ]
+            [ Dropdown.view countryConfig countryContext model.country countries
+            , Dropdown.view cityConfig cityContext model.city cities
+            ]
